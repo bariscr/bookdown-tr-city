ed311de00640d9c715fa1198b6dbdf2b9a921c68
Barış Uçar <barisucar@Barss-MBP.mynet>
2022-02-18 16:08:57 +0300

    africa map

--- code/map-1.R
+++ code/map-1.R
@@ -1,8 +1,11 @@
+library(tidyverse)
 library(tmap)
 data("World")
 
+africa <- World %>% filter(continent == "Africa")
+
 tmap_options(bg.color = "black", legend.text.color = "white")
 
-tm_shape(World) +
+tm_shape(africa) +
   tm_polygons("HPI", legend.title = "Happy Planet Index")
   
